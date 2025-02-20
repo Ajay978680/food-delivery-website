ee237235416cf444dfd262f9a61f16f86d8aa654
Ajay978680 <sparkajay001@gmail.com>
2025-02-20 22:34:19 +0530

    adding list info in Admin panel

--- OneDrive/Desktop/food delivery website/admin/src/pages/Add/Add.jsx
+++ OneDrive/Desktop/food delivery website/admin/src/pages/Add/Add.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import './Add.css'
 import { assets } from '../../assets/assets'
+import axios from "axios"
+import { toast } from 'react-toastify'
 
 const Add = () => {
 
+    const url="http://localhost:4000";
     const[image,SetImage] = useState(false);
     const[data,SetData] = useState({
         name:"",
@@ -20,13 +23,38 @@ const Add = () => {
         }))
     }
 
+    const onSubmitHandler = async(event) =>{
+            event.preventDefault();
+            const formData = new FormData();
+            formData.append("name",data.name)
+            formData.append("description",data.description)
+            formData.append("price",Number(data.price))
+            formData.append("category",data.category)
+            formData.append("image",image)
+            const response = await axios.post(`${url}/api/food/add`,formData);
+            if(response.data.success)
+            {
+                SetData({
+                    name:"",
+                    description:"",
+                    price:"",
+                    category:"Salad"
+                })
+                SetImage(false)
+                toast.success(response.data.message)
+            }
+            else{
+                toast.error(response.data.message)
+            }
+    }
+
     useEffect(()=>{
         console.log(data);
     },[data])
 
   return (
     <div className='add'>
-        <form className='flex-col'>
+        <form className='flex-col' onSubmit={onSubmitHandler}>
             <div className="add-img-upload flex-col">
                 <p>Upload Image</p>
                 <label htmlFor='image'>
