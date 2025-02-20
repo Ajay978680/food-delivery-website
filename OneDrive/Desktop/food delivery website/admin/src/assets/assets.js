a03a580f506b020d6143838b1d6d49513b67ea75
Ajay978680 <sparkajay001@gmail.com>
2025-02-20 12:16:31 +0530

    adding admin section

---
+++ OneDrive/Desktop/food delivery website/admin/src/assets/assets.js
@@ -0,0 +1,20 @@
+import logo from './logo.png'
+import profile_image from './profile_icon.png'
+import add_icon from './add.png'
+import order_icon from './order_icon.png'
+import upload_area from './upload.png'
+import parcel_icon from './parcel_icon.png'
+
+
+
+
+
+export const assets = {
+    logo,
+    profile_image,
+    add_icon,
+    upload_area,
+    order_icon,
+    parcel_icon
+}
+
