52f99d76dc9b63ce23f26053b72e563ebe7c374f
Ajay978680 <sparkajay001@gmail.com>
2025-02-26 15:06:42 +0530

    adding more items

--- OneDrive/Desktop/food delivery website/frontend/src/App.jsx
+++ OneDrive/Desktop/food delivery website/frontend/src/App.jsx
@@ -5,7 +5,6 @@ import Home from './pages/Home/Home';
 import Cart from './pages/Cart/Cart';
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
 import Footer from './components/Footer/Footer';
-import Payment from './pages/Payment/payment';
 import LoginPopup from './components/LoginPopup/LoginPopup';
 
 const App = () => {
@@ -21,7 +20,6 @@ const App = () => {
           <Route path='/' element={<Home />} />
           <Route path='/cart' element={<Cart />} />
           <Route path='/order' element={<PlaceOrder />} />
-          <Route path='/payment' element={<Payment />} />
         </Routes>
       </div>
       <Footer />
