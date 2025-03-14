7fa043ee0c155ae1a8099db2e1afe097aa0867fc
Ajay978680 <sparkajay001@gmail.com>
2025-03-14 23:27:59 +0530

    m

--- OneDrive/Desktop/food delivery website/admin/src/pages/Add/Add.jsx
+++ OneDrive/Desktop/food delivery website/admin/src/pages/Add/Add.jsx
@@ -76,6 +76,7 @@ const Add = ({url}) => {
                             <option value="Pure Veg">Pure Veg</option>
                             <option value="Pasta">Pasta</option>
                             <option value="Noodles">Noodles</option>
+                            <option value="Chicken">Chicken</option>
                         </select>
                     </div>
                     <div className='add-price flex-col'>
